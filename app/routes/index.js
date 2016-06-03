fcfd289b468a7b90698e94bd05b2be66fc20113e
Chase McElroy <ichasem@gmail.com>
2016-06-03 11:45:23 -0700

    created firebase integration so the songs can be stored and updated from the firebase database

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,15 @@
 import Ember from 'ember';
 
-  var songs = [{
-    id: 1,
-    title: 'Grand Old Mansion',
-    lyrics: 'Veruca Salt'
-  }, {
-    id: 2,
-    title: 'Urban Living',
-    lyrics: 'Mike TV'
-  }];
-
+  // var songs = [{
+  //   id: 1,
+  //   title: 'Grand Old Mansion',
+  //   lyrics: 'Veruca Salt'
+  // }, {
+  //   id: 2,
+  //   title: 'Urban Living',
+  //   lyrics: 'Mike TV'
+  // }];
+  //
   export default Ember.Route.extend({
-    model() {
-      return songs;
-    }
+
 });
